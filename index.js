d3570449b55d4837ea316fbcaf0a750368400234
Jesse Kriss <jesse@jklabs.net>
2019-10-17 22:28:45 -0700

    Reload on first load so that the service worker can intercept requests

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', {scope: '/'})
+  .then(function(reg) {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+    // need to reload to load this page with the service worker to get all our features
+    if (!navigator.serviceWorker.controller) window.location.reload()
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  });
+}
+
 const handleFile = function(evt) {
   console.log('file chosen:', evt)
   loadFile(evt.target.files[0])
